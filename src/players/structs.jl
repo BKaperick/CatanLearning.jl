a5e2266e1d9326b78aa6a2ce2db1cbc5e6422b25
Bryan Kaperick <bryankaperick@gmail.com>
2025-02-22 12:23:18 +0100

    Implement non-mutating ML player

--- src/players/structs.jl
+++ src/players/structs.jl
@@ -28,3 +28,24 @@ function Base.deepcopy(player::MutatedEmpathRobotPlayer)
     return MutatedEmpathRobotPlayer(deepcopy(player.player), deepcopy(player.machine), deepcopy(player.mutation)) #TODO needto deepcopy the machine?
 end
 
+mutable struct EmpathRobotPlayer <: RobotPlayer
+    player::Player
+    machine::Machine
+end
+
+EmpathRobotPlayer(team::Symbol) = EmpathRobotPlayer(team, "../../features.csv")
+function EmpathRobotPlayer(team::Symbol, features_file_name::String)
+    Tree = load_tree_model()
+    tree = Base.invokelatest(Tree,
+        max_depth = 6,
+        min_gain = 0.0,
+        min_records = 2,
+        max_features = 0,
+        splitting_criterion = BetaML.Utils.gini)
+    EmpathRobotPlayer(Player(team), try_load_model_from_csv(tree, "$(DATA_DIR)/model.jls", "$(DATA_DIR)/features.csv"))
+end
+
+
+function Base.deepcopy(player::EmpathRobotPlayer)
+    return EmpathRobotPlayer(deepcopy(player.player), player.machine) #TODO needto deepcopy the machine?
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,12 @@ function test_evolving_robot_game(neverend)
     Catan.test_automated_game(neverend, players)
 end
 
+function empath_player()
+    player = EmpathRobotPlayer(:red)
+    board = read_map(SAMPLE_MAP)
+    p = predict_model(player.machine, board, player)
+    return player, board, p
+end
+
 test_evolving_robot_game(neverend)
+
