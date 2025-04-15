d9e4f172826632680cc0a27c4e0818f30365dd33
Bryan Kaperick <bryankaperick@gmail.com>
2025-04-15 15:28:26 +0200

    more cleanup og learning folder

--- src/learning/test_models.jl
+++
@@ -1,7 +0,0 @@
-include("train_model.jl")
-include("../structs.jl")
-include("../players/empath_robot_player.jl")
-
-
-get_row = x -> Vector(mach.data[1][x,1:end])
-tp = x -> _predict_model_feature_vec(mach, get_row(x))
